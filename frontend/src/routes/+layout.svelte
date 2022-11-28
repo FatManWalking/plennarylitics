8a4c3d20e79061d82eb0b902414d1d93c04969d6
Björn Bulkens <bjoern.bulkens@atos.net>
2022-11-28 11:34:05 +0100

    stash cleanup

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+import Nav from "$lib/daisy/navbar.svelte";
+import { onMount } from "svelte";
+import "../app.css";
+
+import { isDark } from '../lib/stores/theme.store';
+  
+  const THEMES = {
+      DARK: 'business',
+      LIGHT: 'garden'
+  };
+
+  onMount(async () => {
+    document.documentElement.setAttribute('data-theme', $isDark ? THEMES.DARK : THEMES.LIGHT);
+    });
+
+</script>
+
+    <Nav/>
+    <slot />
+  

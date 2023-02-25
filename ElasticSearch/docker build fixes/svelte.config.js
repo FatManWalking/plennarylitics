95decdc6d709bda5a851f48f03d6cf2141986f2a
dkeyGit <85196745+dkeyGit@users.noreply.github.com>
2023-02-25 10:44:56 +0100

    docker frontend fix

---
+++ ElasticSearch/docker build fixes/svelte.config.js
@@ -0,0 +1,24 @@
+/*
+import adapter from '@sveltejs/adapter-static';
+export default {
+  kit: {
+    adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+      precompress: false,
+      strict: false
+    })
+  }
+};
+*/
+
+import adapter from '@sveltejs/adapter-vercel';
+
+export default {
+  kit: {
+    adapter: adapter()
+  }
+};
